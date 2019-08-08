d2788d67ce404a0894fd455ca322af650af08874
Virile-Tao <39130294+Virile-Tao@users.noreply.github.com>
2019-08-09 00:37:53 +0800

    Update 奇偶排序.py

--- day025/奇偶排序.py
+++ day025/奇偶排序.py
@@ -1,20 +1,27 @@
-def my_sort(nums: list) -> list:
+import sys
+
+
+def fun():
+    line = sys.stdin.readline().strip()
+    nums = list(map(int, line.split()))
     even = 0
     for num in nums:
         if num % 2 == 0:
             even += 1
     # 全为奇数或全为偶数，无法交换，直接返回
     if even == len(nums) or even == 0:
-        return nums
+        return ' '.join(map(str, nums))
     # 既存在奇数，又存在偶数，快排后返回
     else:
-        return sorted(nums)
-    
-    
-    
-    
-lst1 = [7, 3, 5, 1]
-assert my_sort(lst1) == [7, 3, 5, 1]
+        nums.sort()
+        return ' '.join(map(str, nums))
 
-lst2 = [53941, 38641, 31525, 75864, 29026, 12199, 83522, 58200, 64784, 80987]
-assert my_sort(lst2) == [12199, 29026, 31525, 38641, 53941, 58200, 64784, 75864, 80987, 83522]
+
+if __name__ == '__main__':
+    assert fun() == '7 3 5 1'
+    assert fun() == '12199 29026 31525 38641 53941 58200 64784 75864 80987 83522'
+    assert fun() == '8 4 6 10'
+
+# 输入 7 3 5 1 输出 7 3 5 1 
+# 输入 53941 38641 31525 75864 29026 12199 83522 58200 64784 80987 输出 12199 29026 31525 38641 53941 58200 64784 75864 80987 83522
+# 输入 8 4 6 10 输出 8 4 6 10
