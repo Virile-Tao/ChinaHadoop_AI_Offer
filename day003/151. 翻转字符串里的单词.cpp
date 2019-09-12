1b792853124a0e8557a0bd2082ceac4595138b4c
Virile-Tao <39130294+Virile-Tao@users.noreply.github.com>
2019-09-13 00:21:14 +0800

    Update 151. 翻转字符串里的单词.cpp

--- day003/151. 翻转字符串里的单词.cpp
+++ day003/151. 翻转字符串里的单词.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     string reverseWords(string s) {
+        stringstream ss(s);
+        string tmp, ans;
+        stack<string> stk;
+        while (ss >> tmp)
+        {
+            stk.push(tmp);
+            stk.push(" ");
+        }
+        while (!stk.empty())
+        {
+            ans += stk.top();
+            stk.pop();
+        }
+        return ans.size() ? string(ans.begin()+1, ans.end()) : "";
+    }
+    
+    
+    string reverseWords_v2(string s) {
         int n = s.size();
         if (n==0) return "";
         int i = 0;
